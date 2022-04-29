39161f2a78d153de12a7e7d47a3d83e29859c3de
mwchoen <mwcheon@cudo.co.kr>
2022-04-29 16:53:51 +0900

    상품 상세 페이지

---
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -0,0 +1,48 @@
+package hello.itemservice.web.item.basic;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    // @RequiredArgsConstructor 를 붙이면
+    /**
+     * final 이 붙은 멤버변수만 사용해서 생성자를 자동으로 만들어준다.
+     * public BasicItemController(ItemRepository itemRepository) {
+     * this.itemRepository = itemRepository;
+     * }
+     *
+     * */
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+
+        return "basic/items";
+    }
+
+
+    /**
+     * 테스트 데이터 추가
+     * */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+
+}
