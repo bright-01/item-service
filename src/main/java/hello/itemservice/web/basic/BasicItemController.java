3b89ea636f666a6c32e9a2e834cae6811fa505b7
mwchoen <mwcheon@cudo.co.kr>
2022-05-02 09:19:34 +0900

    상품 목록, 상세

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,11 +1,13 @@
-package hello.itemservice.web.item.basic;
+package hello.itemservice.web.basic;
 
 import hello.itemservice.domain.Item;
 import hello.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -34,6 +36,15 @@ public class BasicItemController {
 
         return "basic/items";
     }
+
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
 
 
     /**
